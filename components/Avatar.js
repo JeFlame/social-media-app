ba669dd69b81f12af98ac8cbb078c88376921be0
Huy Hoàng Vũ <hoangvu130301@gmail.com>
2023-05-31 23:20:12 +0700

    code pages

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,11 +1,11 @@
 export default function Avatar({ size }) {
     let width = 'w-12';
     if (size === 'lg') {
-        width = 'w-36';
+        width = 'w-24 md:w-36';
     }
     return (
         <div className={`${width} rounded-full overflow-hidden`} >
-            <img src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80" alt="" />
+            <img src="https://scontent.fsgn15-1.fna.fbcdn.net/v/t1.6435-9/151184088_3824016630985908_1609593348639139959_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=vs4wflVA1HUAX8nuGPJ&_nc_ht=scontent.fsgn15-1.fna&oh=00_AfCGnVekaZVHjc4q7qtp289qTFFVC8_bRQXSMgZZX9uRQA&oe=649EAA9D" alt="" />
         </div >
     );
 }
--- components/PostCard.js
+++ components/PostCard.js
@@ -29,7 +29,7 @@ export default function PostCard() {
                     <p>
                         <Link href={'/profile'}>
                             <span className="mr-1 font-semibold cursor-pointer hover:underline">
-                                ABCD ABCD
+                                RF_Nhism0suy
                             </span>
                         </Link>
                         shared an <a className="text-socialBlue">album</a>
@@ -116,7 +116,7 @@ export default function PostCard() {
                     <Avatar />
                 </div>
                 <div className="border grow rounded-full relative">
-                    <textarea className="block w-full p-3 px-4 overflow-hidden h-12 rounded-full" placeholder="Leave a comment"></textarea>
+                    <textarea className="block w-full p-3 px-4 overflow-hidden h-12 rounded-full" placeholder="Leave a comment" />
                     <button className="absolute top-3 right-3 text-gray-400">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
